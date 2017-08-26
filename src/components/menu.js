ecade113196f549b80be93c6ea82fd561c8c13d0
Didac Esteban Andres <didacesteban@gmail.com>
2017-08-26 21:08:49 +0200

    first design implementation

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import React from 'react'
+import {Grid, Row, Col} from 'react-bootstrap'
 import {
   BrowserRouter as Router,
   Route,
--- src/domain/models/country.js
+++ src/domain/models/country.js
@@ -1,9 +1,10 @@
 class Country
 {
-  constructor (id, name, temperature) {
+  constructor (id, name, temperature, temperatureFormated) {
     this.id = id;
     this.name = name;
     this.temperature = temperature;
+    this.temperatureFormated = temperatureFormated;
   }
 }
 
@@ -12,7 +13,8 @@ function CountryMapper (json)
   return new Country(
     json.id,
     json.name,
-    json.main.temp
+    json.main.temp,
+    Math.round((json.main.temp * 100)/45)
   );
 }
 
