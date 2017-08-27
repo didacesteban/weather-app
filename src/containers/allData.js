55f5c888c30f918d8d311ebc23a1eb5f279a0660
Didac Esteban Andres <didacesteban@gmail.com>
2017-08-27 17:15:16 +0200

    added table to alldata container

--- src/containers/allData.js
+++ src/containers/allData.js
@@ -22,20 +22,44 @@ class AllData extends Component {
   render() {
     const dataSaved = JSON.parse(`[${localStorage.getItem("allData")}]`);
     let dataMapped = [];
+    let messageNoData = null;
     const creatingDataObject = dataSaved.forEach((array) => {
       dataMapped = _.concat(dataMapped, array);
     });
-    dataMapped = dataMapped.map(country => {
-      return <p>{country.name}</p>
-    });
+    if(localStorage.getItem("allData")){
+      dataMapped = dataMapped.map(country => {
+        return <tr key={country.id}>
+            <td>{country.name}</td>
+            <td>{country.temperature}°</td>
+          </tr>
+      });
+    }else{
+      messageNoData = <p className="text-center">"Sorry, You don't have data saved yet"</p>;
+    }
+
     return <div className="container">
       <Col xs={12} md={12} lg={12}>
         <PageHeader>
-          All time data saved <button onClick={this.clearStorage.bind(this)}>clear data</button>
+          All time data saved
         </PageHeader>
       </Col>
-      <Row className="text-center">
-        {dataMapped}
+      <Row>
+        { localStorage.getItem("allData") ?
+          <Col xs={12} md={12} lg={12} className="text-left">
+            <button className="btn btn-default" onClick={this.clearStorage.bind(this)}>clear data</button>
+            <table className="table table-striped table-allData-margin">
+              <thead>
+                <tr>
+                  <th>Country</th>
+                  <th>Temperature</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataMapped}
+              </tbody>
+            </table>
+          </Col> : {messageNoData}
+        }
       </Row>
     </div>
   }
